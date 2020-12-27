5e856da27399e995712ef827fa6ccf21c07f62d9
RohitKumar-200 <55583961+RohitKumar-200@users.noreply.github.com>
2020-12-27 15:15:04 +0530

    Revert "Edited App.js and added few files in Emotion"

--- src/components/EmotionDetection/Emoji.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Emoji = ({ label, value }) => (
-  <span role="img" aria-label={label}>
-    {value}
-  </span>
-)
-
-export default Emoji
--- src/components/EmotionDetection/ml/tf.js
+++
@@ -1,4 +0,0 @@
-import * as core from '@tensorflow/tfjs-core'
-import * as layers from '@tensorflow/tfjs-layers'
-
-export const tf = { ...core, ...layers }
