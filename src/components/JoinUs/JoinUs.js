7146a16a16649cc46270d7247e2c3d940c182fa7
Mrhb787 <lit2019008@iiitl.ac.in>
2020-12-28 02:25:48 +0530

    Join Us completed

--- src/components/JoinUs/JoinUs.js
+++ src/components/JoinUs/JoinUs.js
@@ -1,9 +1,9 @@
-import React, { useState, useContext } from "react";
+import React, {useState, useContext} from "react";
 import "./JoinUs.css";
-import { UserContext } from "../../Context/userContext";
-import { auth, provider } from "../../firebase";
-import { useHistory } from "react-router-dom";
-import { db } from "../../firebase";
+import {UserContext} from "../../Context/userContext";
+import {auth, provider} from "../../firebase";
+import {useHistory} from "react-router-dom";
+import {db} from "../../firebase";
 
 function JoinUs() {
   const [, setUser] = useContext(UserContext);
@@ -87,55 +87,76 @@ function JoinUs() {
   return (
     <div className="joinUs">
       <div className="joinUs__signInContainer">
+        <div className="google-icon-wrapper">
+          <img
+            className="google-icon"
+            src={`https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg`}
+            alt=""
+            height="20px"
+            width="20px"
+          />
+        </div>
         <button onClick={handleSignIn}>Sign In with google</button>
       </div>
+
       <div className="joinUs__or">or</div>
+
       <div className="joinUs__signUpContainer">
         <div className="joinUs__signUpHeading">Sign Up</div>
         <form onSubmit={handleSignUp}>
-          <div className="joinUs__signUpDOB">
-            DOB
-            <input
-              type="date"
-              value={DOB}
-              onChange={(e) => setDOB(e.target.value)}
-            />
-          </div>
-          <div
-            className="joinUs__signUpGender"
-            onChange={(e) => setGender(e.target.value)}
-          >
-            Gender
-            <input type="radio" id="male" name="gender" value="Male" />
-            <label for="male">Male</label>
-            <input type="radio" id="female" name="gender" value="Female" />
-            <label for="female">Female</label>
-            <input type="radio" id="other" name="gender" value="Other" />
-            <label for="other">Other</label>
-          </div>
-          <div className="joinUs__signUpBloodGroup">
-            <label for="bloodGroup">Blood group</label>
-            <select
-              name="bloodGroup"
-              value={bloodGroup}
-              onChange={(e) => setBloodGroup(e.target.value)}
+          <fieldset>
+            <div className="joinUs__signUpDOB">
+              <p>DOB : </p>
+              <input
+                type="date"
+                value={DOB}
+                onChange={(e) => setDOB(e.target.value)}
+              />
+            </div>
+            <div
+              className="joinUs__signUpGender"
+              onChange={(e) => setGender(e.target.value)}
             >
-              <option value="">Select</option>
-              <option value="A+">A+</option>
-              <option value="A-">A-</option>
-              <option value="B+">B+</option>
-              <option value="B-">B-</option>
-              <option value="O+">O+</option>
-              <option value="O-">O-</option>
-              <option value="AB+">AB+</option>
-              <option value="AB-">AB-</option>
-            </select>
-          </div>
-          <div className="joinUs__signUpSubmitContainer">
-            <button type="submit" onClick={handleSignUp}>
-              Sign Up with google
-            </button>
-          </div>
+              Gender
+              <input type="radio" id="male" name="gender" value="Male" />
+              <label for="male">Male</label>
+              <input type="radio" id="female" name="gender" value="Female" />
+              <label for="female">Female</label>
+              <input type="radio" id="other" name="gender" value="Other" />
+              <label for="other">Other</label>
+            </div>
+            <div className="joinUs__signUpBloodGroup">
+              <label for="bloodGroup">Blood group</label>
+              <select
+                name="bloodGroup"
+                value={bloodGroup}
+                onChange={(e) => setBloodGroup(e.target.value)}
+              >
+                <option value="0">Select</option>
+                <option value="A+">A+</option>
+                <option value="A-">A-</option>
+                <option value="B+">B+</option>
+                <option value="B-">B-</option>
+                <option value="O+">O+</option>
+                <option value="O-">O-</option>
+                <option value="AB+">AB+</option>
+                <option value="AB-">AB-</option>
+              </select>
+            </div>
+            <div className="joinUs__signUpSubmitContainer">
+              <div className="joinUs_google-icon-wrapper">
+                <img
+                  src={`https://s3-us-west-2.amazonaws.com/s.cdpn.io/14082/icon_google.png`}
+                  alt=""
+                  height="18px"
+                  width="18px"
+                />
+              </div>
+              <button type="submit" onClick={handleSignUp}>
+                Sign Up with google
+              </button>
+            </div>
+          </fieldset>
         </form>
       </div>
     </div>
